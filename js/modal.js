7e7665b28a0afbfd75cfecf90ce788fc30ade494
Yuliya Kozina <ykozina@yandex.ru>
2026-09-22 18:51:57 +0300

    Внесла правки по дз 5 и 6 урока

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,15 @@
 const modal = document.querySelector('.popup')
 const modalPopup = modal.querySelector('.popup-content')
 const button = document.querySelector('.form-button')
+const cancelButton = document.querySelector('.form-button-cancel')
 
 
 button.addEventListener('click', () => {
     modal.style.display = "block"
+})
+
+cancelButton.addEventListener('click', () => {
+    modal.style.display = 'none'
 })
 
 modal.addEventListener('click', (e) => {
